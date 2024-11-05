eb104803883db6170ae9e1cf5da07c47f590b2de
Gyemin-Jeon <asdjeon@naver.com>
2024-11-05 16:14:42 +0900

    20241105

---
+++ 05_array/src/pkg03_one_dimension/ex/Ex01.java
@@ -0,0 +1,38 @@
+package pkg03_one_dimension.ex;
+
+public class Ex01 {
+
+  public static void main(String[] args) {
+ 
+    // 3단 구구단의 결과 (3, 6, 9, ... , 27) -> 배열에 저장하기
+
+    for(int a = 1; a <= 9; a++) {
+      System.out.println("3 * " + a + " = " + (3 * a));
+    }
+    
+    String[] result;
+    result = new String[9];
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+  }
+
+}
---
+++ 05_array/src/pkg03_one_dimension/ex/Ex04.java
@@ -0,0 +1,35 @@
+package pkg03_one_dimension.ex;
+
+public class Ex04 {
+
+  public static void main(String[] args) {
+ 
+    // 10진수를 2진수로 변환한 결과를 배열에 저장하기
+
+    // 10진수
+    int number = 35;
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+  }
+
+}
