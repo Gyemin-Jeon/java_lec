6179f9e3192406f71a05eadf36df460c2fb2775a
Gyemin-Jeon <asdjeon@naver.com>
2024-11-20 13:19:34 +0900

    20241119

---
+++ 13_lambda/src/pkg02_Functional_Interface/Class03_Function.java
@@ -0,0 +1,32 @@
+package pkg02_Functional_Interface;
+
+import java.util.function.Function;
+
+public class Class03_Function {
+
+  /* 
+   * package java.util.function;
+   * 
+   * @FunctionalInterface
+   * public interface Function<T R> {
+   *    R apply(T t);
+   * }
+   */
+  
+  public static void main(String[] args) {
+    
+    // Lambda Expression
+    
+    Function<String, Integer> function = str -> Integer.parseInt(str);
+    System.out.println(function.apply("10"));
+    
+    // Function 타입의 매개변수에 Lambda Expression 전달
+    a(name -> name + "님");
+    
+  }
+
+  public static void a(Function<String, String> function) {
+    System.out.println(function.apply("yiyi"));
+  }
+  
+}
